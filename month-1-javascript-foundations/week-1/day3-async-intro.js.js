bf3851442e4ca727e7855fe001bcd5e7bdb2c0fc
DonJuan2077 <Orion21age@gmail.com>
2025-09-22 00:35:56 +0800

    refactor: simplify day3 code

--- month-1-javascript-foundations/week-1/day3-async-intro.js.js
+++ month-1-javascript-foundations/week-1/day3-async-intro.js.js
@@ -1,11 +1,20 @@
-
+console.log("订单1: 开始");
 
 setTimeout(() => {
   console.log("订单3: 开始吃0毫秒的超大汉堡 (耗时任务开始)");
   
-}, 2)
+  // 这段代码会空转3秒，模拟一个耗时的计算任务
+  const startTime = Date.now();
+  while (Date.now() - startTime < 3000) {
+    // 循环3秒钟
+  }
+  
+  console.log("订单3: 终于吃完了汉堡 (耗时任务结束)");
+}, 0);
 
 setTimeout(() => {
   console.log("订单4: 1毫秒的普通可乐好了");
 }, 1);
+
+console.log("订单2: 结束");
 
