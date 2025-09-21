9b778fe2dfd95d29809d6235d43bb18add2468c2
DonJuan2077 <Orion21age@gmail.com>
2025-09-21 19:08:23 +0800

    refactor: improve comments in day2 functions

--- month-1-javascript-foundations/week-1/day2-equality-functions.js
+++ month-1-javascript-foundations/week-1/day2-equality-functions.js
@@ -1,16 +1,6 @@
-console.log("--- 字符串与数字 ---");
-console.log("5 == '5'  :", 5 == "5");
-console.log("5 === '5' :", 5 === "5");
+console.log("5 == '5'  :", 5 == "5"); // print true
+console.log("5 === '5' :", 5 === "5"); // print false
 
-console.log("\n--- 布尔值与数字 ---");
-console.log("0 == false  :", 0 == false);
-console.log("0 === false :", 0 === false);
-console.log("1 == true   :", 1 == true);
-console.log("1 === true  :", 1 === true);
-
-console.log("\n--- null 与 undefined ---");
-console.log("null == undefined  :", null == undefined);
-console.log("null === undefined :", null === undefined);
 
 function square(x) 
 {
@@ -18,8 +8,13 @@ function square(x)
 }
 
 const squareArrow = (x) => x * x;
+const squareArrow2 = (x) => 
+{
+    return x * x;
+}
 
 let n = 2;
 
 console.log(square(n));
 console.log(squareArrow(n));
+console.log(squareArrow2(n));
