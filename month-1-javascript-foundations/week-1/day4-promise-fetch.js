69f6f0f5e02d5d832ab5d8ae8890787f30156fc6
DonJuan2077 <Orion21age@gmail.com>
2025-09-23 19:57:58 +0800

    refactor: improve from day3 to day4 all works

--- month-1-javascript-foundations/week-1/day4-promise-fetch.js
+++ month-1-javascript-foundations/week-1/day4-promise-fetch.js
@@ -1,18 +1,29 @@
-const apiUrl = 'https://jsonplaceholder.typicode.com/todos/1';
-
-console.log("正在发送请求...");
-
-fetch(apiUrl)
-  .then(response => {
-    console.log("收到服务器响应，正在解析...");
-    return response.json(); 
-  })
-  .then(data => {
-    console.log("解析完成！获取到的数据是:");
-    console.log(data);
-  })
-  .catch(error => {
-    console.error("请求失败:", error);
+const createOrder = (shouldSucceed) => {
+  return new Promise((resolve, reject) => {
+    
+    console.log("订单处理中... (状态: pending)");
+    
+    setTimeout(() => {
+      if (shouldSucceed) {
+        // 如果操作成功，我们调用 resolve() 函数
+        // 这会将 Promise 的状态从 pending 变为 fulfilled
+        resolve("包裹已送达！(状态: fulfilled)"); 
+      } else {
+        // 如果操作失败，我们调用 reject() 函数
+        // 这会将 Promise 的状态从 pending 变为 rejected
+        reject("配送失败：地址不存在。(状态: rejected)");
+      }
+    }, 2000); 
   });
+};
 
-console.log("请求已发送，主线程继续执行...");
+
+const successfulOrder = createOrder(true); 
+
+successfulOrder
+  .then((successMessage) => {
+    console.log("快递员发来消息:", successMessage);
+  })
+  .catch((errorMessage) => {
+    console.error("快递员发来消息:", errorMessage);
+  });
